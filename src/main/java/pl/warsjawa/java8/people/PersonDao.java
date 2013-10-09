93e2b73641ae435baf0a3d15fb81605e7f0494aa
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-10-09 23:07:40 +0200

    Clean up

--- src/main/java/pl/warsjawa/java8/people/PersonDao.java
+++ src/main/java/pl/warsjawa/java8/people/PersonDao.java
@@ -6,21 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 public class PersonDao {
 
 	public List<Person> loadPeopleDatabase() {
 		try (BufferedReader bufferedReader = open("/people.csv")) {
-			return bufferedReader.lines().
-					filter(line -> !line.startsWith("#")).
-					map(this::parseLine).
-					filter(p -> !p.getName().isEmpty()).
-					sorted(Comparator.comparing(Person::getName).thenComparing(Person::getDateOfBirth)).
-					collect(toList());
+			return Collections.emptyList();
+			// return bufferedReader.lines().
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
