836e63bf15c6e0c3cac615d84a0c920b369d1bbc
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-04-27 13:37:20 +0200

    String.chars() test

---
+++ src/test/java/com/nurkiewicz/java8/Lesson04b_StringStreamsTest.java
@@ -0,0 +1,36 @@
+package com.nurkiewicz.java8;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * - String.chars()
+ */
+public class Lesson04b_StringStreamsTest {
+
+	@Test
+	public void removeNonDigitsFromPhoneNumber() throws Exception {
+		//given
+		final String phone = "+12 345-678 (90)";
+
+		//when
+		final String sanitized = "";  //phone.chars()...
+
+		//then
+		assertThat(sanitized).isEqualTo("1234567890");
+	}
+
+	@Test
+	public void shouldTurnCamelCaseToUnderscores() throws Exception {
+		//given
+		final String input = "thisIsVariable";
+
+		//when
+		final String result = "";  //input.chars()...
+
+		//then
+		assertThat(result).isEqualTo("this_is_variable");
+	}
+
+}
