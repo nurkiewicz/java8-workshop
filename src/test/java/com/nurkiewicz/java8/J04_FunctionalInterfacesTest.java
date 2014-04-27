2be0fd246aabb4fb4fd03fe663d9488ab123a19b
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-04-27 22:27:02 +0200

    Better functional interfaces test

--- src/test/java/com/nurkiewicz/java8/J04_FunctionalInterfacesTest.java
+++ src/test/java/com/nurkiewicz/java8/J04_FunctionalInterfacesTest.java
@@ -3,18 +3,20 @@ package com.nurkiewicz.java8;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.Random;
 import java.util.function.Supplier;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
- * - What is a functional interface? @FunctionalInterface
- * - Using lambdas instead of plain old Java classes (JButton)
+ * What is a functional interface? @FunctionalInterface
+ * Using lambdas instead of plain old Java classes (JButton)
+ * - Implement functional interfaces using lambda syntax
  */
 @Ignore
 public class J04_FunctionalInterfacesTest {
@@ -23,35 +25,33 @@ public class J04_FunctionalInterfacesTest {
 
 	@Test
 	public void testActionListenerLambda() {
-		JButton button = new JButton();
-		button.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println(e.getModifiers());
-				System.out.println(e.getActionCommand());
-			}
-		});
+		//given
+		final Date dateMock = mock(Date.class);
+		final ActionListener listener = null;
+
+		//when
+		listener.actionPerformed(null);
+
+		//then
+		verify(dateMock).setTime(1000L);
 	}
 
 	@Test
 	public void testRunnableLambda() {
-		Runnable run = new Runnable() {
+		//given
+		final Date dateMock = mock(Date.class);
+		Runnable block = null;
 
-			@Override
-			public void run() {
-				System.out.println("Runnable!");
-			}
-		};
+		//when
+		block.run();
+
+		//then
+		verify(dateMock).setTime(1000L);
 	}
 
 	@Test
 	public void testComparatorLambda() {
-		final Comparator<String> strLenComparator = new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return Integer.compare(o1.length(), o2.length());
-			}
-		};
+		final Comparator<String> strLenComparator = null;
 
 		assertThat(strLenComparator.compare("abc", "def")).isZero();
 		assertThat(strLenComparator.compare("abc", "defg")).isLessThan(0);
@@ -60,12 +60,7 @@ public class J04_FunctionalInterfacesTest {
 
 	@Test
 	public void testCustomFunctionalInterface() {
-		final RandomSource source = new RandomSource() {
-			@Override
-			public int oneOrMinusOne() {
-				return random.nextInt(2) * 2 - 1;
-			}
-		};
+		final RandomSource source = null;
 
 		Supplier<Integer> sourceSupplier = null;
 
