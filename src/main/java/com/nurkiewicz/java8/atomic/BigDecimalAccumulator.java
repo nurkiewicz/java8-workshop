204a47e74fdb62bf65edff3ea00fb229dae64666
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-05-01 22:38:46 +0200

    Less hints in default implementation

--- src/main/java/com/nurkiewicz/java8/atomic/BigDecimalAccumulator.java
+++ src/main/java/com/nurkiewicz/java8/atomic/BigDecimalAccumulator.java
@@ -6,7 +6,6 @@ import java.util.function.BiFunction;
 public class BigDecimalAccumulator {
 
 	private final BiFunction<BigDecimal, BigDecimal, BigDecimal> accFun;
-	private final BigDecimal[] accumulators;
 
 	public BigDecimalAccumulator(BigDecimal initialValue, BiFunction<BigDecimal, BigDecimal, BigDecimal> accFun) {
 		this(initialValue, accFun, availCpuMin4());
@@ -14,10 +13,6 @@ public class BigDecimalAccumulator {
 
 	public BigDecimalAccumulator(BigDecimal initialValue, BiFunction<BigDecimal, BigDecimal, BigDecimal> accFun, int concurrency) {
 		this.accFun = accFun;
-		this.accumulators = new BigDecimal[concurrency];
-		for (int i = 0; i < accumulators.length; i++) {
-			accumulators[i] = initialValue;
-		}
 	}
 
 	private static int availCpuMin4() {
