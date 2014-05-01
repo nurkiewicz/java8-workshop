89f2a4a387a2c1f207335eed9fb1c06d1bffa6fc
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-05-01 20:16:14 +0200

    More tests

--- src/test/java/com/nurkiewicz/java8/J08_LocalDateTest.java
+++ src/test/java/com/nurkiewicz/java8/J08_LocalDateTest.java
@@ -6,10 +6,17 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.stream.Stream;
 
+import static java.time.Month.DECEMBER;
 import static java.time.Month.MAY;
+import static java.time.Month.SEPTEMBER;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 @Ignore
@@ -33,6 +40,21 @@ public class J08_LocalDateTest {
 		assertThat(numberOfHolidays).isEqualTo(113);
 	}
 
+	/**
+	 * @see TemporalAdjusters
+	 */
+	@Test
+	public void shouldApplyBuiltIntTemporalAdjuster() throws Exception {
+		//given
+		final LocalDate today = LocalDate.of(2014, MAY, 12);
+
+		//when
+		final LocalDate previousWednesday = today;
+
+		//then
+		assertThat(previousWednesday).isEqualTo(LocalDate.of(2014, MAY, 7));
+	}
+
 	@Test
 	public void shouldApplyCustomTemporalAdjuster() throws Exception {
 		//given
@@ -47,6 +69,33 @@ public class J08_LocalDateTest {
 
 	public TemporalAdjuster nextHoliday() {
 		throw new UnsupportedOperationException("nextHoliday()");
+	}
+
+	/**
+	 * - Calculate time after billion seconds from given date
+	 * - What will be the hour in  Asia/Tokyo"
+	 * - See how period can be calculated
+	 * @throws Exception
+	 */
+	@Test
+	public void shouldFindWhenIhaveBillionthSecondBirthday() throws Exception {
+		//given
+		final LocalDate dateOfBirth = LocalDate.of(1985, DECEMBER, 25);
+		final LocalTime timeOfBirth = LocalTime.of(22, 10);
+		final ZonedDateTime birth = ZonedDateTime.of(dateOfBirth, timeOfBirth, ZoneId.of("Europe/Warsaw"));
+
+		//when
+		final ZonedDateTime billionSecondsLater = birth;
+		final int hourInTokyo = billionSecondsLater.getHour();
+
+		//then
+		final Period periodToBillionth = Period.between(
+				LocalDate.of(2014, MAY, 12),
+				billionSecondsLater.toLocalDate());
+		assertThat(billionSecondsLater.toLocalDate()).isEqualTo(LocalDate.of(2017, SEPTEMBER, 3));
+		assertThat(hourInTokyo).isEqualTo(7);
+		assertThat(periodToBillionth.getYears()).isEqualTo(3);
+		assertThat(periodToBillionth.getMonths()).isEqualTo(3);
 	}
 
 }
