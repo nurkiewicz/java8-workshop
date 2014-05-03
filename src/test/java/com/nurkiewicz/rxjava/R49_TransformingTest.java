00dffa50cb6cc9b7be466964444ae5b027673794
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-05-03 20:06:57 +0200

    More Rx exercises

---
+++ src/test/java/com/nurkiewicz/rxjava/R49_TransformingTest.java
@@ -0,0 +1,68 @@
+package com.nurkiewicz.rxjava;
+
+import org.junit.Test;
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class R49_TransformingTest {
+
+	@Test
+	public void shouldGroupItemsInBatchesOfSameSize() throws Exception {
+		//given
+		final Observable<Integer> observable = Observable.range(1, 10);
+
+		//when
+		final Observable<List<Integer>> grouped = observable.buffer(3, 3);
+
+		//then
+		final Iterable<List<Integer>> groups = grouped.toBlockingObservable().toIterable();
+		assertThat(groups).containsExactly(
+				Arrays.asList(1, 2, 3),
+				Arrays.asList(4, 5, 6),
+				Arrays.asList(7, 8, 9),
+				Arrays.asList(10)
+		);
+	}
+
+	@Test
+	public void shouldGenerateSlidingWindowOverItems() throws Exception {
+		//given
+		final Observable<Integer> observable = Observable.range(1, 5);
+
+		//when
+		final Observable<List<Integer>> grouped = observable.buffer(3, 1);
+
+		//then
+		final Iterable<List<Integer>> groups = grouped.toBlockingObservable().toIterable();
+
+		groups.forEach(System.out::println);
+		assertThat(groups).containsExactly(
+				Arrays.asList(1, 2, 3),
+				Arrays.asList(2, 3, 4),
+				Arrays.asList(3, 4, 5),
+				Arrays.asList(4, 5),
+				Arrays.asList(5)
+		);
+	}
+
+	@Test
+	public void shouldAccumulateValues() throws Exception {
+		//given
+		final Observable<Integer> observable = Observable.range(1, 4);
+
+		//when
+		final Observable<Integer> accumulated = observable.scan((x, y) -> x + y);
+
+		//then
+		final Iterable<Integer> result = accumulated.toBlockingObservable().toIterable();
+		assertThat(result).containsExactly(
+				1,
+				1 + 2,
+				1 + 2 + 3,
+				1 + 2 + 3 + 4);
+	}
+}
---
+++ src/test/java/com/nurkiewicz/rxjava/R50_OperatorsTest.java
@@ -0,0 +1,86 @@
+package com.nurkiewicz.rxjava;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import rx.Observable;
+import rx.Subscriber;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class R50_OperatorsTest {
+
+	@Test
+	public void shouldRejectItemsNotGreaterThanMaxSoFar() throws Exception {
+		//given
+		final Observable<Integer> observable = Observable.from(1, 2, 3, 2, 5, 4, 7, 5, 6, 7, 8, 9, 5, 6, 9, 10, 9, 12);
+
+		//when
+		final Observable<Integer> maxObservable = observable;       //TODO
+
+		//then
+		final List<Integer> filtered = Lists.newArrayList(maxObservable.toBlockingObservable().toIterable());
+		assertThat(filtered).containsExactly(1, 2, 3, 5, 7, 8, 9, 10, 12);
+	}
+
+	@Test
+	public void shouldFilterUsingCustomOperator() throws Exception {
+		final Observable<Integer> observable = Observable.from(1, 2, 3, 2, 5, 4, 7, 5, 6, 7, 8, 9, 5, 6, 9, 10, 9, 12);
+
+		//when
+		final Observable<Integer> maxObservable = observable.lift(myFilter(x -> x < 5));
+
+		//then
+		final List<Integer> filtered = Lists.newArrayList(maxObservable.toBlockingObservable().toIterable());
+		assertThat(filtered).containsExactly(1, 2, 3, 2, 4);
+	}
+
+	private Observable.Operator<Integer, Integer> myFilter(Predicate<Integer> predicate) {
+		return new MyFilterOperator(predicate);
+	}
+
+	@Test
+	public void shouldRejectItemsNotGreaterThanMaxSoFarUsingCustomOperator() throws Exception {
+		//given
+		final Observable<Integer> observable = Observable.from(1, 2, 3, 2, 5, 4, 7, 5, 6, 7, 8, 9, 5, 6, 9, 10, 9, 12);
+
+		//when
+		final Observable<Integer> maxObservable = observable.lift(onlyGreater());
+
+		//then
+		final List<Integer> filtered = Lists.newArrayList(maxObservable.toBlockingObservable().toIterable());
+		assertThat(filtered).containsExactly(1, 2, 3, 5, 7, 8, 9, 10, 12);
+	}
+
+	private Observable.Operator<Integer, Integer> onlyGreater() {
+		throw new UnsupportedOperationException("onlyGreater()");
+	}
+
+}
+
+class MyFilterOperator implements Observable.Operator<Integer, Integer> {
+	public MyFilterOperator(Predicate<Integer> predicate) {
+	}
+
+	@Override
+	public Subscriber<? super Integer> call(Subscriber<? super Integer> subscriber) {
+		return new Subscriber<Integer>() {
+			@Override
+			public void onCompleted() {
+				subscriber.onCompleted();
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				subscriber.onError(e);
+			}
+
+			@Override
+			public void onNext(Integer integer) {
+				subscriber.onNext(integer);
+			}
+		};
+	}
+}
