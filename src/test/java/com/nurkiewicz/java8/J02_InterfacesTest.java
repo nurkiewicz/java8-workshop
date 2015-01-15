8ba069889bf8df48cc7a726ec2c10a28face6208
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2015-01-15 23:55:13 +0100

    Replace JUnit's @Parametrized with JUnitParams
    JUnitParams is much less verbose.

--- src/test/java/com/nurkiewicz/java8/J02_InterfacesTest.java
+++ src/test/java/com/nurkiewicz/java8/J02_InterfacesTest.java
@@ -4,46 +4,28 @@ import com.nurkiewicz.java8.interfaces.Encrypter;
 import com.nurkiewicz.java8.interfaces.ReverseEncrypter;
 import com.nurkiewicz.java8.interfaces.RotEncrypter;
 import com.nurkiewicz.java8.interfaces.XorEncrypter;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.Collection;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static junitparams.JUnitParamsRunner.$;
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.junit.runners.Parameterized.Parameters;
 
 /**
  * - Improve encrypter to use default methods to avoid code duplication.
  */
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class J02_InterfacesTest {
 
-	@Parameters
-	public static Collection<Object[]> encrypterAndExpected() {
-		Object[][] data = new Object[][]{
-				{new ReverseEncrypter(), new byte[]{-91, -101, -111}},
-				{new RotEncrypter(), new byte[]{103, 113, 123}},
-				{new XorEncrypter(), new byte[]{-106, -88, -94}}
-		};
-		return Arrays.asList(data);
-	}
-
-	private final Encrypter encrypter;
-	private final byte[] expected;
-
-	public J02_InterfacesTest(Encrypter encrypter, byte[] expected) {
-		this.encrypter = encrypter;
-		this.expected = expected;
-	}
-
 	@Test
-	public void testAllMethods() throws IOException {
+	@Parameters
+	public void testAllMethods(Encrypter encrypter, byte[] expected) throws IOException {
 		final byte[] input = new byte[]{90, 100, 110};
 		final char[] charInput = new char[]{'Z', 'd', 'n'};
 
@@ -54,4 +36,11 @@ public class J02_InterfacesTest {
 		assertThat(encrypter.encode(new ByteArrayInputStream(input))).isEqualTo(expected);
 	}
 
+	private Object[] parametersForTestAllMethods() {
+		return $(
+				$(new ReverseEncrypter(), new byte[]{-91, -101, -111}),
+				$(new RotEncrypter(), new byte[]{103, 113, 123}),
+				$(new XorEncrypter(), new byte[]{-106, -88, -94})
+		);
+	}
 }
