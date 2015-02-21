b05cdb9bf777942e6755157661c390171002cc16
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-02-21 19:51:45 +0100

    Examples of custom ForkJoinPool

--- src/test/java/com/nurkiewicz/java8/J10_ParallelStreamsTest.java
+++ src/test/java/com/nurkiewicz/java8/J10_ParallelStreamsTest.java
@@ -2,10 +2,16 @@ package com.nurkiewicz.java8;
 
 import com.nurkiewicz.java8.people.Person;
 import com.nurkiewicz.java8.people.PersonDao;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -18,6 +24,17 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class J10_ParallelStreamsTest {
 
 	private final PersonDao dao = new PersonDao();
+	private ForkJoinPool pool;
+
+	@Before
+	public void setupPool() {
+		pool = new ForkJoinPool(8);
+	}
+
+	@After
+	public void shutdownPool() {
+		pool.shutdownNow();
+	}
 
 	@Test
 	public void parallel() throws IOException {
@@ -31,6 +48,38 @@ public class J10_ParallelStreamsTest {
 				.collect(toList());
 
 		assertThat(veryTallParallel).isEqualTo(veryTall);
+	}
+
+	/**
+	 * Doesn't test much, just a demo.
+	 */
+	@Test
+	public void runningInCustomForkJoinPool() throws IOException, ExecutionException, InterruptedException {
+		List<Integer> ints = IntStream
+				.range(0, 20)
+				.mapToObj(Integer::valueOf)
+				.collect(toList());
+
+		pool.submit(() -> {
+			ints.parallelStream().forEach(i ->
+					System.out.println(Thread.currentThread().getName() + ": " + i));
+		}).get();
+	}
+
+	@Test
+	public void parallelFilterKeepsOrder() throws IOException, ExecutionException, InterruptedException {
+		List<Integer> ints = IntStream
+				.range(0, 20)
+				.mapToObj(Integer::valueOf)
+				.collect(toList());
+
+		ForkJoinTask<List<Integer>> future = pool.submit(() -> {
+			return ints
+					.parallelStream()
+					.filter(i -> i % 6 == 0)
+					.collect(toList());
+		});
+		assertThat(future.get()).containsExactly(0, 6, 12, 18);
 	}
 
 
